8501a9a5f4919e7b796f8a18c9607e4c117c1516
TFIDHB <m1a.k0.rar@gmail.com>
2026-03-18 16:53:59 +0300

    [US-1] Database Connection Configured Database Connection

---
+++ DAL/AuthDbContext.cs
@@ -0,0 +1,27 @@
+﻿using DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL
+{
+    public class AuthDbContext : DbContext
+    {
+        public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.HasIndex(e => e.Email).IsUnique();
+                entity.Property(e => e.Email).IsRequired();
+                entity.Property(e => e.PasswordHash).IsRequired();
+            }
+            );
+        }
+    }
+}
--- InnoClinic.Auth.API/Program.cs
+++ InnoClinic.Auth.API/Program.cs
@@ -1,3 +1,6 @@
+using DAL;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +8,13 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<AuthDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("AuthConnection"),
+        sqlOptions => sqlOptions.MigrationsAssembly("InnoClinic.Auth.API")
+    )
+);
 
 var app = builder.Build();
 
