b1c32f8b6ccd9453a959790b59ec13a492e648ea
TFIDHB <m1a.k0.rar@gmail.com>
2026-05-20 16:28:54 +0300

    [T-2] Added endpoints

--- InnoClinic/src/Services.API/Services.API.API/Controllers/ServicesController.cs
+++ InnoClinic/src/Services.API/Services.API.API/Controllers/ServicesController.cs
@@ -1,11 +1,59 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Application.DTOs;
+using Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Services.API.Controllers
 {
     //[Authorize]
     [ApiController]
     [Route("api/v1/services")]
-    public class ServicesController : ControllerBase
+    public class ServicesController(IServicesService servicesService) : ControllerBase
     {
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<ServiceDto>> GetService(Guid id, CancellationToken ct = default)
+        {
+            var result = await servicesService.GetByIdAsync(id, ct);
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<IEnumerable<ServiceDto>>> GetAllServices(CancellationToken ct = default) 
+        {
+            var result = await servicesService.GetAllAsync(ct);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        public async Task<ActionResult<ServiceDto>> CreateService([FromBody] CreateServiceRequestDto dto, CancellationToken ct = default)
+        {
+            var result = await servicesService.CreateAsync(dto, ct);
+            return CreatedAtAction(nameof(GetService), new {id = result.Id}, result);
+        }
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<ServiceDto>> UpdateService(Guid id, [FromBody] UpdateServiceRequestDto dto, CancellationToken ct = default)
+        {
+            var result = await servicesService.UpdateAsync(id, dto, ct);
+            return Ok(result);
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<ServiceDto>> DeleteService(Guid id, CancellationToken ct = default)
+        {
+            await servicesService.DeleteAsync(id, ct);
+            return Ok();
+        }
     }
 }
