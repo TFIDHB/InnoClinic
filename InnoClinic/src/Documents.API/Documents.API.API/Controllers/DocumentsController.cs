b6537cdf61883184876baa6883a08924245fb1bf
TFIDHB <m1a.k0.rar@gmail.com>
2026-06-15 16:03:26 +0300

    [T-4] Implemented controllers Configured appsettings and user secrets

---
+++ InnoClinic/src/Documents.API/Documents.API.API/Controllers/DocumentsController.cs
@@ -0,0 +1,57 @@
+﻿using Application.DTOs;
+using Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Documents.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/documents")]
+    public class DocumentsController(IDocumentsService documentsService) : ControllerBase
+    {
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<DocumentDto>> GetDocument(Guid id, CancellationToken ct = default)
+        {
+            var result = await documentsService.GetByIdAsync(id, ct);
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        public async Task<ActionResult<IEnumerable<DocumentDto>>> GetAllDocuments(CancellationToken ct = default)
+        {
+            var result = await documentsService.GetAllAsync(ct);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        public async Task<ActionResult<DocumentDto>> UploadDocument([FromForm] UploadDocumentRequestDto dto, CancellationToken ct = default)
+        {
+            var result = await documentsService.UploadAsync(dto, ct);
+            return CreatedAtAction(nameof(GetDocument), new { id = result.Id }, result);
+        }
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<DocumentDto>> UpdateDocument(Guid id, [FromForm] UpdateDocumentRequestDto dto, CancellationToken ct = default)
+        {
+            var result = await documentsService.UpdateAsync(id, dto, ct);
+            return Ok(result);
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult> DeleteDocument(Guid id, CancellationToken ct = default)
+        {
+            await documentsService.DeleteAsync(id, ct);
+            return Ok();
+        }
+    }
+}
--- InnoClinic/src/Documents.API/Documents.API.API/Program.cs
+++ InnoClinic/src/Documents.API/Documents.API.API/Program.cs
@@ -1,21 +1,28 @@
+using Application.Extensions;
+using Documents.API.Extensions;
+using Infrastructure.Extensions;
+using InnoClinic.Shared.Middleware;
+using InnoClinic.Shared.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddPresentation();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseAppSwagger("InnoClinic.Documents.API");
 }
+
+app.UseMiddleware<GlobalExceptionMiddleware>();
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
